0928e75602df352d8f358c3fe891e86574dfbc6e
choo8 <joshua.choo.1993@gmail.com>
2025-09-21 19:16:51 +0800

    Implemented instructions to pass Corax+ opcode test

--- src/chip8/instruction.rs
+++ src/chip8/instruction.rs
@@ -5,16 +5,72 @@ use super::types::Nibble;
 pub enum Instruction {
     // 00E0 - CLS
     Clear,
+    // 00EE - RET
+    Return,
     // 1nnn - JP addr
     Jump(u16),
+    // 2nnn - CALL addr
+    Call(u16),
+    // 3xkk - SE Vx, byte
+    SkipEqual(RegisterIndex, u8),
+    // 4xkk - SNE Vx, byte
+    SkipNotEqual(RegisterIndex, u8),
+    // 5xy0 - SE Vx, Vy
+    SkipEqualRegister(RegisterIndex, RegisterIndex),
     // 6xkk - LD Vx, byte
     Load(RegisterIndex, u8),
     // 7xkk - ADD Vx, byte
     Add(RegisterIndex, u8),
+    // 8xy0 - LD Vx, Vy
+    LoadRegister(RegisterIndex, RegisterIndex),
+    // 8xy1 - OR Vx, Vy
+    LoadOr(RegisterIndex, RegisterIndex),
+    // 8xy2 - AND Vx, Vy
+    LoadAnd(RegisterIndex, RegisterIndex),
+    // 8xy3 - XOR Vx, Vy
+    LoadXor(RegisterIndex, RegisterIndex),
+    // 8xy4 - ADD Vx, Vy
+    LoadAdd(RegisterIndex, RegisterIndex),
+    // 8xy5 - SUB Vx, Vy
+    LoadSub(RegisterIndex, RegisterIndex),
+    // 8xy6 - SHR Vx
+    LoadShiftRight(RegisterIndex),
+    // 8xy7 - SUBN Vx, Vy
+    LoadSubNegative(RegisterIndex, RegisterIndex),
+    // 8xyE - SHL Vx
+    LoadShiftLeft(RegisterIndex),
+    // 9xy0 - SNE Vx, Vy
+    SkipNotEqualRegister(RegisterIndex, RegisterIndex),
     // Annn - LD I, addr
     LoadIndexRegister(u16),
+    // Bnnn - JP V0, addr
+    JumpWithOffset(u16),
+    // Cxkk - RND Vx, byte
+    Random(RegisterIndex, u8),
     // Dxyn - DRW Vx, Vy, nibble
     Display(RegisterIndex, RegisterIndex, Nibble),
+    // Ex9E - SKP Vx
+    SkipKeyPress(RegisterIndex),
+    // ExA1 - SKNP Vx
+    SkipKeyNotPress(RegisterIndex),
+    // Fx07 - LD Vx, DT
+    LoadDelayTimer(RegisterIndex),
+    // Fx0A - LD Vx, K
+    LoadKeyPress(RegisterIndex),
+    // Fx15 - LD DT, Vx
+    StoreDelayTimer(RegisterIndex),
+    // Fx18 - LD ST, Vx
+    StoreSoundTimer(RegisterIndex),
+    // Fx1E - ADD I, Vx
+    AddIndexRegister(RegisterIndex),
+    // Fx29 - LD F, Vx
+    LoadFontCharacter(RegisterIndex),
+    // Fx33 - LD B, Vx
+    LoadBinaryCodedDecimal(RegisterIndex),
+    // Fx55 - LD [I], Vx
+    StoreRegisters(RegisterIndex),
+    // Fx65 - LD Vx, [I]
+    LoadRegisters(RegisterIndex),
 }
 
 impl From<u16> for Instruction {
@@ -22,10 +78,35 @@ impl From<u16> for Instruction {
         let opcode = (raw_instruction & 0xF000) >> 12;
 
         match opcode {
-            0x0 => Instruction::Clear,
+            0x0 => {
+                if raw_instruction == 0x00E0 {
+                    Instruction::Clear
+                } else {
+                    Instruction::Return
+                }
+            }
             0x1 => {
                 let nnn = raw_instruction & 0x0FFF;
                 Instruction::Jump(nnn)
+            }
+            0x2 => {
+                let nnn = raw_instruction & 0x0FFF;
+                Instruction::Call(nnn)
+            }
+            0x3 => {
+                let x = ((raw_instruction & 0x0F00) >> 8) as u8;
+                let kk = (raw_instruction & 0x00FF) as u8;
+                Instruction::SkipEqual(RegisterIndex::try_from(x).unwrap(), kk)
+            }
+            0x4 => {
+                let x = ((raw_instruction & 0x0F00) >> 8) as u8;
+                let kk = (raw_instruction & 0x00FF) as u8;
+                Instruction::SkipNotEqual(RegisterIndex::try_from(x).unwrap(), kk)
+            }
+            0x5 => {
+                let x = ((raw_instruction & 0x0F00) >> 8) as u8;
+                let y = ((raw_instruction & 0x00F0) >> 4) as u8;
+                Instruction::SkipEqualRegister(RegisterIndex::try_from(x).unwrap(), RegisterIndex::try_from(y).unwrap())
             }
             0x6 => {
                 let x = ((raw_instruction & 0x0F00) >> 8) as u8;
@@ -37,15 +118,71 @@ impl From<u16> for Instruction {
                 let kk = (raw_instruction & 0x00FF) as u8;
                 Instruction::Add(RegisterIndex::try_from(x).unwrap(), kk)
             }
+            0x8 => {
+                let x = ((raw_instruction & 0x0F00) >> 8) as u8;
+                let y = ((raw_instruction & 0x00F0) >> 4) as u8;
+                let subcode = raw_instruction & 0x000F;
+                match subcode {
+                    0x0 => Instruction::LoadRegister(RegisterIndex::try_from(x).unwrap(), RegisterIndex::try_from(y).unwrap()),
+                    0x1 => Instruction::LoadOr(RegisterIndex::try_from(x).unwrap(), RegisterIndex::try_from(y).unwrap()),
+                    0x2 => Instruction::LoadAnd(RegisterIndex::try_from(x).unwrap(), RegisterIndex::try_from(y).unwrap()),
+                    0x3 => Instruction::LoadXor(RegisterIndex::try_from(x).unwrap(), RegisterIndex::try_from(y).unwrap()),
+                    0x4 => Instruction::LoadAdd(RegisterIndex::try_from(x).unwrap(), RegisterIndex::try_from(y).unwrap()),
+                    0x5 => Instruction::LoadSub(RegisterIndex::try_from(x).unwrap(), RegisterIndex::try_from(y).unwrap()),
+                    0x6 => Instruction::LoadShiftRight(RegisterIndex::try_from(x).unwrap()),
+                    0x7 => Instruction::LoadSubNegative(RegisterIndex::try_from(x).unwrap(), RegisterIndex::try_from(y).unwrap()),
+                    0xE => Instruction::LoadShiftLeft(RegisterIndex::try_from(x).unwrap()),
+                    _ => panic!("Unknown instruction: {:#X}", raw_instruction)
+                }
+            }
+            0x9 => {
+                let x = ((raw_instruction & 0x0F00) >> 8) as u8;
+                let y = ((raw_instruction & 0x00F0) >> 4) as u8;
+                Instruction::SkipNotEqualRegister(RegisterIndex::try_from(x).unwrap(), RegisterIndex::try_from(y).unwrap())
+            }
             0xA => {
                 let nnn = raw_instruction & 0x0FFF;
                 Instruction::LoadIndexRegister(nnn)
+            }
+            0xB => {
+                let nnn = raw_instruction & 0x0FFF;
+                Instruction::JumpWithOffset(nnn)
+            }
+            0xC => {
+                let x = ((raw_instruction & 0x0F00) >> 8) as u8;
+                let kk = (raw_instruction & 0x00FF) as u8;
+                Instruction::Random(RegisterIndex::try_from(x).unwrap(), kk)
             }
             0xD => {
                 let x = ((raw_instruction & 0x0F00) >> 8) as u8;
                 let y = ((raw_instruction & 0x00F0) >> 4) as u8;
                 let n = (raw_instruction & 0x000F) as u8;
                 Instruction::Display(RegisterIndex::try_from(x).unwrap(), RegisterIndex::try_from(y).unwrap(), Nibble::try_from(n).unwrap())
+            }
+            0xE => {
+                let x = ((raw_instruction & 0x0F00) >> 8) as u8;
+                let subcode = raw_instruction & 0x00FF;
+                match subcode {
+                    0x9E => Instruction::SkipKeyPress(RegisterIndex::try_from(x).unwrap()),
+                    0xA1 => Instruction::SkipKeyNotPress(RegisterIndex::try_from(x).unwrap()),
+                    _ => panic!("Unknown instruction: {:#X}", raw_instruction)
+                }
+            }
+            0xF => {
+                let x = ((raw_instruction & 0x0F00) >> 8) as u8;
+                let subcode = raw_instruction & 0x00FF;
+                match subcode {
+                    0x7 => Instruction::LoadDelayTimer(RegisterIndex::try_from(x).unwrap()),
+                    0x0A => Instruction::LoadKeyPress(RegisterIndex::try_from(x).unwrap()),
+                    0x15 => Instruction::StoreDelayTimer(RegisterIndex::try_from(x).unwrap()),
+                    0x18 => Instruction::StoreSoundTimer(RegisterIndex::try_from(x).unwrap()),
+                    0x1E => Instruction::AddIndexRegister(RegisterIndex::try_from(x).unwrap()),
+                    0x29 => Instruction::LoadFontCharacter(RegisterIndex::try_from(x).unwrap()),
+                    0x33 => Instruction::LoadBinaryCodedDecimal(RegisterIndex::try_from(x).unwrap()),
+                    0x55 => Instruction::StoreRegisters(RegisterIndex::try_from(x).unwrap()),
+                    0x65 => Instruction::LoadRegisters(RegisterIndex::try_from(x).unwrap()),
+                    _ => panic!("Unknown instruction: {:#X}", raw_instruction)
+                }
             }
             _ => panic!("Unknown instruction: {:#X}", raw_instruction),
         }
--- src/chip8/mod.rs
+++ src/chip8/mod.rs
@@ -11,6 +11,8 @@ use keypad::Keypad;
 use memory::Memory;
 use register::{RegisterIndex, RegisterFile};
 
+use crate::chip8::memory::FONTSET_START_ADDRESS;
+
 pub struct Chip8 {
     memory: Memory,
     registers: RegisterFile,
@@ -55,22 +57,100 @@ impl Chip8 {
             Instruction::Clear => {
                 self.display.clear();
             }
+            Instruction::Return => {
+                self.stack_pointer -= 1;
+                self.program_counter = self.stack[self.stack_pointer as usize];
+            }
             Instruction::Jump(addr) => {
                 self.program_counter = addr;
             }
-            Instruction::Load(vx, kk) => {
-                self.registers.set(vx, kk);
+            Instruction::Call(addr) => {
+                self.stack[self.stack_pointer as usize] = self.program_counter;
+                self.stack_pointer += 1;
+                self.program_counter = addr;
             }
-            Instruction::Add(vx, kk) => {
-                let current = self.registers.get(vx);
-                self.registers.set(vx, current.wrapping_add(kk));
+            Instruction::SkipEqual(x, kk, ) => {
+                if self.registers.get(x) == kk {
+                    self.program_counter += 2;
+                }
+            }
+            Instruction::SkipNotEqual(x, kk) => {
+                if self.registers.get(x) != kk {
+                    self.program_counter += 2;
+                }
+            }
+            Instruction::SkipEqualRegister(x, y, ) => {
+                if self.registers.get(x) == self.registers.get(y) {
+                    self.program_counter += 2;
+                }
+            }
+            Instruction::Load(x, kk) => {
+                self.registers.set(x, kk);
+            }
+            Instruction::Add(x, kk) => {
+                let current = self.registers.get(x);
+                self.registers.set(x, current.wrapping_add(kk));
+            }
+            Instruction::LoadRegister(x, y, ) => {
+                let value = self.registers.get(y);
+                self.registers.set(x, value);
+            }
+            Instruction::LoadOr(x, y) => {
+                let value = self.registers.get(x) | self.registers.get(y);
+                self.registers.set(x, value);
+            }
+            Instruction::LoadAnd(x, y) => {
+                let value = self.registers.get(x) & self.registers.get(y);
+                self.registers.set(x, value);
+            }
+            Instruction::LoadXor(x, y) => {
+                let value = self.registers.get(x) ^ self.registers.get(y);
+                self.registers.set(x, value);
+            }
+            Instruction::LoadAdd(x, y) => {
+                let (result, carry) = self.registers.get(x).overflowing_add(self.registers.get(y));
+                self.registers.set(x, result);
+                self.registers.set(RegisterIndex::try_from(0xF).unwrap(), if carry { 1 } else { 0 });
+            }
+            Instruction::LoadSub(x, y) => {
+                let (result, borrow) = self.registers.get(x).overflowing_sub(self.registers.get(y));
+                self.registers.set(x, result);
+                self.registers.set(RegisterIndex::try_from(0xF).unwrap(), if borrow { 0 } else { 1 });
+            }
+            Instruction::LoadShiftRight(x) => {
+                let lsb = self.registers.get(x) & 0x1;
+                self.registers.set(RegisterIndex::try_from(0xF).unwrap(), lsb);
+                self.registers.set(x, self.registers.get(x) >> 1);
+            }
+            Instruction::LoadSubNegative(x, y) => {
+                let (result, borrow) = self.registers.get(y).overflowing_sub(self.registers.get(x));
+                self.registers.set(x, result);
+                self.registers.set(RegisterIndex::try_from(0xF).unwrap(), if borrow { 0 } else { 1 });
+            }
+            Instruction::LoadShiftLeft(x) => {
+                let msb = (self.registers.get(x) & 0x80) >> 7;
+                self.registers.set(RegisterIndex::try_from(0xF).unwrap(), msb);
+                self.registers.set(x, self.registers.get(x) << 1);
+            }
+            Instruction::SkipNotEqualRegister(x, y) => {
+                if self.registers.get(x) != self.registers.get(y) {
+                    self.program_counter += 2;
+                }
             }
             Instruction::LoadIndexRegister(addr) => {
                 self.index_register = addr;
             }
-            Instruction::Display(x_reg, y_reg, nibble) => {
-                let x = self.registers.get(x_reg) as usize;
-                let y = self.registers.get(y_reg) as usize;
+            Instruction::JumpWithOffset(addr) => {
+                let offset = self.registers.get(RegisterIndex::try_from(0).unwrap()) as u16;
+                self.program_counter = addr + offset;
+            }
+            Instruction::Random(x, kk) => {
+                let random_byte: u8 = rand::random();
+                self.registers.set(x, random_byte & kk);
+            }
+            Instruction::Display(x, y, nibble) => {
+                let x = self.registers.get(x) as usize;
+                let y = self.registers.get(y) as usize;
                 let height = nibble.value() as usize;
 
                 self.registers.set(RegisterIndex::try_from(0xF).unwrap(), 0);
@@ -86,6 +166,63 @@ impl Chip8 {
                             }
                         }
                     }
+                }
+            }
+            Instruction::SkipKeyPress(x) => {
+                let key = self.registers.get(x);
+                if self.keypad.is_key_pressed(key) {
+                    self.program_counter += 2;
+                }
+            }
+            Instruction::SkipKeyNotPress(x) => {
+                let key = self.registers.get(x);
+                if !self.keypad.is_key_pressed(key) {
+                    self.program_counter += 2;
+                }
+            }
+            Instruction::LoadDelayTimer(x) => {
+                let value = self.delay_timer;
+                self.registers.set(x, value);
+            }
+            Instruction::LoadKeyPress(x) => {
+                if let Some(key) = self.keypad.get_pressed_key() {
+                    self.registers.set(x, key);
+                } else {
+                    self.program_counter -= 2; // Repeat this instruction
+                }
+            }
+            Instruction::StoreDelayTimer(x) => {
+                let value = self.registers.get(x);
+                self.delay_timer = value;
+            }
+            Instruction::StoreSoundTimer(x) => {
+                let value = self.registers.get(x);
+                self.sound_timer = value;
+            }
+            Instruction::AddIndexRegister(x) => {
+                let value = self.registers.get(x) as u16;
+                self.index_register = self.index_register.wrapping_add(value);
+            }
+            Instruction::LoadFontCharacter(x) => {
+                let value = self.registers.get(x);
+                self.index_register = FONTSET_START_ADDRESS as u16 + (value as u16 * 5);
+            }
+            Instruction::LoadBinaryCodedDecimal(x) => {
+                let value = self.registers.get(x);
+                self.memory.write_byte(self.index_register, (value / 100) % 10);
+                self.memory.write_byte(self.index_register + 1, (value % 100) / 10);
+                self.memory.write_byte(self.index_register + 2, value % 10);
+            }
+            Instruction::StoreRegisters(x) => {
+                for i in 0..=x.value() {
+                    let value = self.registers.get(RegisterIndex::try_from(i as u8).unwrap());
+                    self.memory.write_byte(self.index_register + i as u16, value);
+                }
+            }
+            Instruction::LoadRegisters(x) => {
+                for i in 0..=x.value() {
+                    let value = self.memory.read_byte(self.index_register + i as u16);
+                    self.registers.set(RegisterIndex::try_from(i as u8).unwrap(), value);
                 }
             }
         }
